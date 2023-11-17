33b6bf1c0a9abcb92300d20efccfcacaed42b84b
zeyad rashed <brwnboy2002@gmail.com>
2023-11-17 12:28:49 -0500

    chore: project init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # zmBook
+
 Compile markdown files into books and websites. Like mdBook but written in Java.
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module xyz.zuner.zmbook {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+        
+    opens xyz.zuner.zmbook to javafx.fxml;
+    exports xyz.zuner.zmbook;
+}
