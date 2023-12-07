add235b0161c54946cfdcd48be04261573d599c4
zeyad rashed <brwnboy2002@gmail.com>
2023-12-07 15:18:09 -0500

    feat: begun work on visitor interface for markdown AST

---
+++ markdown-processor/src/main/java/xyz/zuner/markdownprocessor/ast/Visitor.java
@@ -0,0 +1,9 @@
+package xyz.zuner.markdownprocessor.ast;
+
+/**
+ * A visitor interface for traversing the AST nodes.
+ */
+public interface Visitor {
+
+    // More visit methods for different node types.
+}
