014c0e4c6ab954d3235a52afac3f3651692f0512
zeyad rashed <brwnboy2002@gmail.com>
2023-12-07 21:15:03 -0500

    fix: add visit method

--- markdown-processor/src/main/java/xyz/zuner/markdownprocessor/ast/Visitor.java
+++ markdown-processor/src/main/java/xyz/zuner/markdownprocessor/ast/Visitor.java
@@ -1,9 +1,12 @@
 package xyz.zuner.markdownprocessor.ast;
 
+import xyz.zuner.markdownprocessor.ast.impl.TextNode;
+
 /**
  * A visitor interface for traversing the AST nodes.
  */
 public interface Visitor {
 
+    void visit(TextNode node);
     // More visit methods for different node types.
 }
